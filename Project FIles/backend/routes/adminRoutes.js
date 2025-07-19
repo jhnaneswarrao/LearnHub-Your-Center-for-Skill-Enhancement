c396e3b04f5c955771ffaf3de18e9dae1d744bd5
jhnaneswarrao <jhnaneswarraobobbili2712@gmail.com>
2025-07-19 10:15:35 +0530

    Create adminRoutes.js

---
+++ Project FIles/backend/routes/adminRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { adminDashboard } = require('../controllers/adminController');
+const router = express.Router();
+
+router.get('/dashboard', adminDashboard);
+
+module.exports = router;
